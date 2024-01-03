fix an error with variance and std. dev.

sdf-net/app/evaluate_octree.py
@@ -48,8 +48,7 @@ def sample_near_surface_2(
     """
     distrib = area_weighted_distribution(V, F)
     samples = sample_surface(V, F, num_samples, distrib)[0]
-    variance = noise_std ** 2
-    samples += torch.randn_like(samples) * variance
+    samples += torch.randn_like(samples) * noise_std
     return samples
 
 

sdf-net/app/spc/SPCDataset.py
@@ -128,7 +128,7 @@ class SPCDataset(Dataset):
             near = sample_near_surface(self.net.V.cuda(), 
                                     self.net.F.cuda(), 
                                     size, 
-                                    variance=1.0/(2**level))
+                                    std_dev=1.0/(2**level))
             trace = sample_surface(self.net.V.cuda(),
                                 self.net.F.cuda(),
                                 size)[0]

sdf-net/lib/datasets/MeshDataset.py
@@ -84,8 +84,7 @@ class MeshDataset(Dataset):
 
             noise_std=1e-2
             pts_surf, _ = sample_surface(self.V, self.F, self.num_samples*5)
-            variance = noise_std ** 2
-            pts_surf += torch.randn_like(pts_surf) * variance
+            pts_surf += torch.randn_like(pts_surf) * noise_std
 
             self.pts = torch.cat((self.pts, pts_surf))
 

sdf-net/lib/torchgp/sample_near_surface.py
@@ -28,7 +28,7 @@ def sample_near_surface(
     V : torch.Tensor,
     F : torch.Tensor, 
     num_samples: int, 
-    variance : float = 0.01,
+    std_dev : float = 0.01,
     distrib=None):
     """Sample points near the mesh surface.
 
@@ -41,5 +41,5 @@ def sample_near_surface(
     if distrib is None:
         distrib = area_weighted_distribution(V, F)
     samples = sample_surface(V, F, num_samples, distrib)[0]
-    samples += torch.randn_like(samples) * variance
+    samples += torch.randn_like(samples) * std_dev
     return samples