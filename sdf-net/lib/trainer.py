d2a67159acbcec7a7648ff6c6c9780aad189ca48
michael-2956 <work.michael.2956@gmail.com>
2024-01-04 13:26:19 +0200

    fix dataset

--- sdf-net/lib/trainer.py
+++ sdf-net/lib/trainer.py
@@ -219,13 +219,15 @@ class Trainer(object):
         self.timer.check('create_dataloader')
         log.info("Loaded mesh dataset")
 
-        self.near_surf_ds = OccupancyTestingDataset(self.args, sample_mode='near')
+        num_tests = 100000
+
+        self.near_surf_ds = OccupancyTestingDataset(self.args, num_tests, self.args.mesh_dataset, sample_mode='near')
         log.info("Near Surface Size: {}".format(len(self.near_surf_ds)))
         self.near_surf_dl = DataLoader(self.near_surf_ds, batch_size=self.args.batch_size, 
                                             shuffle=False, pin_memory=True, num_workers=0)
         log.info("Loaded Near Surface Dataset")
 
-        self.uniform_ds = OccupancyTestingDataset(self.args, sample_mode='rand')
+        self.uniform_ds = OccupancyTestingDataset(self.args, num_tests, self.args.mesh_dataset, sample_mode='rand')
         log.info("Near Surface Size: {}".format(len(self.uniform_ds)))
         self.uniform_dl = DataLoader(self.uniform_ds, batch_size=self.args.batch_size, 
                                             shuffle=False, pin_memory=True, num_workers=0)
