4c37fa23e899d9085ca0573f55c65eeaee7d8e52
michael-2956 <work.michael.2956@gmail.com>
2024-01-04 13:30:34 +0200

    debug

--- sdf-net/lib/trainer.py
+++ sdf-net/lib/trainer.py
@@ -221,13 +221,13 @@ class Trainer(object):
 
         num_tests = 100000
 
-        self.near_surf_ds = OccupancyTestingDataset(self.args, num_tests, self.train_dataset, sample_mode='near')
+        self.near_surf_ds = OccupancyTestingDataset(self.args, num_tests, self.args.dataset_path, sample_mode='near')
         log.info("Near Surface Size: {}".format(len(self.near_surf_ds)))
         self.near_surf_dl = DataLoader(self.near_surf_ds, batch_size=self.args.batch_size, 
                                             shuffle=False, pin_memory=True, num_workers=0)
         log.info("Loaded Near Surface Dataset")
 
-        self.uniform_ds = OccupancyTestingDataset(self.args, num_tests, self.train_dataset, sample_mode='rand')
+        self.uniform_ds = OccupancyTestingDataset(self.args, num_tests, self.args.dataset_path, sample_mode='rand')
         log.info("Near Surface Size: {}".format(len(self.uniform_ds)))
         self.uniform_dl = DataLoader(self.uniform_ds, batch_size=self.args.batch_size, 
                                             shuffle=False, pin_memory=True, num_workers=0)
